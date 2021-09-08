df43892006c72507c75e7179529166cbdb80c0e7
felipeferreiira <88400571+felipeferreiira@users.noreply.github.com>
2021-09-08 18:58:59 -0300

    configurado firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pawflow/modules/login/login_page.dart';
-import 'package:pawflow/modules/splash/splash_page.dart';
-import 'package:pawflow/shared/themes/app_colors.dart';
+import 'package:pawflow/shared/app_widget.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(AppFirebase());
 }
 
-class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+class AppFirebase extends StatefulWidget {
+  const AppFirebase({Key? key}) : super(key: key);
+
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: LoginPage(),
-    );
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return Material(
+                child: Center(
+              child: Text(
+                'Não foi possivel inicializar o FireBase',
+                textDirection: TextDirection.ltr,
+              ),
+            ));
+          }
+
+          // Once complete, show your application
+          else if (snapshot.connectionState == ConnectionState.done) {
+            return AppWidget();
+          } else {
+            return Material(
+              child: Center(child: CircularProgressIndicator()),
+            );
+          }
+        });
   }
 }
