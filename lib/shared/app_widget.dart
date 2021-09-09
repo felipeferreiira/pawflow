c251445f60d64d084dafa273f473da47fd87c3c0
felipeferreiira <88400571+felipeferreiira@users.noreply.github.com>
2021-09-09 13:35:48 -0300

    :white_check_mark:

--- lib/shared/app_widget.dart
+++ lib/shared/app_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pawflow/modules/home/home_page.dart';
 import 'package:pawflow/modules/login/login_page.dart';
+import 'package:pawflow/modules/splash/splash_page.dart';
 import 'package:pawflow/shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
@@ -10,7 +12,13 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: LoginPage(),
+      initialRoute: "/splash", //seleciona como primeira pagina
+      routes: {
+        //rotas nomeadas das paginas
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
